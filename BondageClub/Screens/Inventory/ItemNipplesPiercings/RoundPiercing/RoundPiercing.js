1b086e92601ed012cf6146f5fcd65b5a8b736caf
SandrineBC <61221552+SandrineBC@users.noreply.github.com>
2020-08-29 13:38:08 +0200

    JSDoc finalised

--- BondageClub/Screens/Inventory/ItemNipplesPiercings/RoundPiercing/RoundPiercing.js
+++ BondageClub/Screens/Inventory/ItemNipplesPiercings/RoundPiercing/RoundPiercing.js
@@ -82,6 +82,15 @@ function InventoryItemNipplesPiercingsRoundPiercingPublishAction(C, Option, Prev
 	ChatRoomPublishCustomAction(msg, true, Dictionary);
 }
 
+/**
+ * The NPC dialog is for what the NPC says to you when you make a change to their restraints - the dialog lookup is on a 
+ * per-NPC basis. You basically put the "AssetName" + OptionName in there to allow individual NPCs to override their default 
+ * "GroupName" dialog if for example we ever wanted an NPC to react specifically to having the restraint put on them. 
+ * That could be done by adding an "AssetName" entry (or entries) to that NPC's dialog CSV
+ * @param {Character} C - The NPC to whom the restraint is applied
+ * @param {Option} Option - The chosen option for this extended item
+ * @returns {void} - Nothing
+ */
 function InventoryItemNipplesPiercingsRoundPiercingNpcDialog(C, Option) {
-	C.CurrentDialog = DialogFind(C, "ItemNipplesPiercingsRoundPiercing" + Option.Name, "ItemNipplesPiercings");
+	C.CurrentDialog = DialogFind(C, "RoundPiercingNPCReaction" + Option.Name, "ItemNipplesPiercings");
 }
