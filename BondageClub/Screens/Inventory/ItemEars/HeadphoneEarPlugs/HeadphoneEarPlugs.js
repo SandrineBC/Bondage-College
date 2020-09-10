77c9902c3b10c2a90e63f553b92488932d0553b5
SandrineBC <sascha.boetzel@web.de>
2020-09-10 16:07:55 +0200

    JSDoc added

--- BondageClub/Screens/Inventory/ItemEars/HeadphoneEarPlugs/HeadphoneEarPlugs.js
+++ BondageClub/Screens/Inventory/ItemEars/HeadphoneEarPlugs/HeadphoneEarPlugs.js
@@ -43,11 +43,21 @@ function InventoryItemEarsHeadphoneEarPlugsDraw() {
 }
 
 
-// Catches the item extension clicks
+/**
+ * Catches the item extension clicks
+ * @returns {void} - Nothing
+ */
 function InventoryItemEarsHeadphoneEarPlugsClick() {
 	ExtendedItemClick(InventoryItemEarsHeadphoneEarPlugsOptions);
 }
 
+/**
+ * Publishes the message to the chat
+ * @param {Character} C - The target character
+ * @param {Option} Option - The currently selected Option
+ * @param {Option} PreviousOption - The previously selected Option
+ * @returns {void} - Nothing
+ */
 function InventoryItemEarsHeadphoneEarPlugsPublishAction(C, Option, PreviousOption) {
 	var NewIndex = InventoryItemEarsHeadphoneEarPlugsOptions.indexOf(Option);
 	var PreviousIndex = InventoryItemEarsHeadphoneEarPlugsOptions.indexOf(PreviousOption);
@@ -60,46 +70,16 @@ function InventoryItemEarsHeadphoneEarPlugsPublishAction(C, Option, PreviousOpti
 	ChatRoomPublishCustomAction(msg, true, Dictionary);
 }
 
+/**
+ * The NPC dialog is for what the NPC says to you when you make a change to their restraints - the dialog lookup is on a 
+ * per-NPC basis. You basically put the "AssetName" + OptionName in there to allow individual NPCs to override their default 
+ * "GroupName" dialog if for example we ever wanted an NPC to react specifically to having the restraint put on them. 
+ * That could be done by adding an "AssetName" entry (or entries) to that NPC's dialog CSV
+ * @param {Character} C - The NPC to whom the restraint is applied
+ * @param {Option} Option - The chosen option for this extended item
+ * @returns {void} - Nothing
+ */
 function InventoryItemEarsHeadphoneEarPlugsNpcDialog(C, Option) {
 	C.CurrentDialog = DialogFind(C, "ItemArmsHeadphoneEarPlugs" + Option.Name, "ItemEars");
 }
 
-
-// // Sets the cuffs pose (wrist, elbow, both or none)
-// function InventoryItemEarsHeadphoneEarPlugsSetPose(NewPose) {
-
-// 	// Gets the current item and character
-// 	var C = (Player.FocusGroup != null) ? Player : CurrentCharacter;
-// 	if ((CurrentScreen == "ChatRoom") || (DialogFocusItem == null)) {
-// 		DialogFocusItem = InventoryGet(C, C.FocusGroup.Name);
-// 		InventoryItemEarsHeadphoneEarPlugsLoad();
-// 	} 
-
-// 	// Sets the new pose with it's effects
-// 	DialogFocusItem.Property.Restrain = NewPose;
-// 	if (NewPose == null) {
-// 		delete DialogFocusItem.Property.Effect;
-// 		delete DialogFocusItem.Property.Type;
-// 	} else {
-// 		DialogFocusItem.Property.Effect = [""];
-// 		DialogFocusItem.Property.Type = NewPose;
-// 		if (NewPose == "Light") DialogFocusItem.Property.Effect = ["DeafLight"];
-// 		if (NewPose == "Heavy") DialogFocusItem.Property.Effect = ["DeafHeavy"];
-// 	}
-
-// 	// Refreshes the character and chatroom
-// 	CharacterRefresh(C);
-// 	var msg = "HeadphoneEarPlugsRestrain" + ((NewPose == null) ? "None" : NewPose);
-// 	var Dictionary = [];
-// 	Dictionary.push({Tag: "SourceCharacter", Text: Player.Name, MemberNumber: Player.MemberNumber});
-// 	Dictionary.push({Tag: "DestinationCharacter", Text: C.Name, MemberNumber: C.MemberNumber});
-// 	Dictionary.push({Tag: "AssetName", Text: DialogFocusItem.Asset.Description.toLowerCase()});
-// 	ChatRoomPublishCustomAction(msg, true, Dictionary);
-
-// 	// Rebuilds the inventory menu
-// 	if (DialogInventory != null) {
-// 		DialogFocusItem = null;
-// 		DialogMenuButtonBuild(C);
-// 	}
-
-// }
