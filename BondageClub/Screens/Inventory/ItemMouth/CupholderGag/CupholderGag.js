a2581ecbe13390ff2235aa69f8eb729e313ca743
SandrineBC <sascha.boetzel@web.de>
2020-12-21 17:21:01 +0100

    strange behavior

--- BondageClub/Screens/Inventory/ItemMouth/CupholderGag/CupholderGag.js
+++ BondageClub/Screens/Inventory/ItemMouth/CupholderGag/CupholderGag.js
@@ -2,21 +2,18 @@
 
 const InventoryItemMouthCupholderGagOptions = [
 	{
-		Name: "RemoveCup",
-		Property: {
-			Type: "RemoveCup",
+		Name: "NoCup",
+		Property: { Type: "",
 		},
 	},
 	{
-		Name: "TipOverCup",
-		Property: {
-			Type: "TipOverCup",
+		Name: "Tip",
+		Property: { Type: "",
 		},
 	},
 	{
-		Name: "PlaceCup",
-		Property: {
-			Type: "Cup",
+		Name: "Cup",
+		Property: { Type: "Cup",
 		},
 	},
 ];
@@ -34,7 +31,7 @@ function InventoryItemMouthCupholderGagLoad() {
  * @returns {void} - Nothing
  */
 function InventoryItemMouthCupholderGagDraw() {
-	ExtendedItemDraw(InventoryItemMouthCupholderGagOptions, "CupholderGag", InventoryItemMouthCupholderGagOptions.length, false);
+	ExtendedItemDraw(InventoryItemMouthCupholderGagOptions, "CupholderGagOptions", InventoryItemMouthCupholderGagOptions.length, false);
 }
 
 /**
@@ -50,7 +47,6 @@ function InventoryItemMouthCupholderGagClick() {
  * @param {Character} C - The character who get restrained
  * @param {Option} Option - The extended item option
  * @param {string} Option.Name - The name of the option. Used for finding the right dialog entry
- * @param {Option} PreviousOption - The extended item option that was previously used. Not used in this item
  * @returns {void} - Nothing
  */
 function InventoryItemMouthCupholderGagPublishAction(C, Option) {
@@ -78,79 +74,26 @@ function InventoryItemMouthCupholderGagNpcDialog(C, Option) {
 /**
  * Validate function that checks, if the restrained character wears outer clothes. If so, the duct tape cannot be applied
  * @param {Character} C - The character wearing the item
- * @param {Option} Option - The option to be applied on the character. Not used in this item
+ * @param {Option} Option - The option to be applied on the character. 
  * @returns {boolean} - Returns false and sets DialogExtendedMessage, if the chosen option is not possible.
  */
-function InventoryItemMouthCupholderGagValidate(C, Option) {
+/* function InventoryItemMouthCupholderGagValidate(C, Option) {
 	var Allowed = "";
 
-	console.log(Option.Name);
-
 	switch (Option.Name) {
-		case "RemoveCup":
-		case "TipOverCup":
-			if (!Option.Property.Type === "Cup") Allowed = "No cup present";
+		case "NoCup":
+		case "Tip":
+			if (!DialogFocusItem.Property.Type) {
+				Allowed = DialogFind(Player, "CupHolderGagNoCupPresent");
+			}
 			break;
-		case "PlaceCup":
-			if (Option.Property.Type === "Cup") Allowed = "Cup already present";
+		case "Cup":
+			if (DialogFocusItem.Property.Type) {
+				Allowed = DialogFind(Player, "CupHolderGagCupAlreadyPresent");
+			}
 			break;
 	}// switch
 
 	return Allowed;
 }
-
-
-/*
-// Draw the item extension screen
-function InventoryItemMouthCupholderGagDraw() {
-
-	// Draw the header and item
-	DrawRect(1387, 125, 225, 275, "white");
-	DrawImageResize("Assets/" + DialogFocusItem.Asset.Group.Family + "/" + DialogFocusItem.Asset.Group.Name + "/Preview/" + DialogFocusItem.Asset.Name + ".png", 1389, 127, 221, 221);
-	DrawTextFit(DialogFocusItem.Asset.Description, 1500, 375, 221, "black");
-	
-	DrawText(DialogFind(Player, "CupholderGagOptions"), 1500, 500, "white", "gray");
-	DrawButton(1200, 650, 200, 55, DialogFind(Player, "CupholderGagTipOverCup"), DialogFocusItem.Property.Type == "Cup" ? "White" : "#888888");
-	DrawButton(1550, 650, 200, 55, DialogFind(Player, "CupholderGagRemoveCup"), Player.CanInteract() && DialogFocusItem.Property.Type == "Cup" ? "White" : "#888888");
-	DrawButton(1375, 710, 200, 55, DialogFind(Player, "CupholderGagPlaceCup"), Player.CanInteract() && !DialogFocusItem.Property.Type ? "White" : "#888888");
-
-}
-*/
-
-/* // Catches the item extension clicks
-function InventoryItemMouthCupholderGagClick() {
-	if ((MouseX >= 1885) && (MouseX <= 1975) && (MouseY >= 25) && (MouseY <= 110)) DialogFocusItem = null;
-	if ((MouseX >= 1325) && (MouseX <= 1389) && (MouseY >= 800) && (MouseY <= 864) && (CurrentScreen == "ChatRoom")) {
-		DialogFocusItem.Property.ShowText = !DialogFocusItem.Property.ShowText;
-		DialogLeave();
-	}
-	
-	// Click mode depending on who interacts
-	if ((MouseX >= 1200) && (MouseX <= 1400) && (MouseY >= 650) && (MouseY <= 705) && (DialogFocusItem.Property.Type == "Cup")) InventoryItemMouthCupholderGagSetType("Tip");
-	if ((MouseX >= 1550) && (MouseX <= 1750) && (MouseY >= 650) && (MouseY <= 705) && (Player.CanInteract() && DialogFocusItem.Property.Type == "Cup")) InventoryItemMouthCupholderGagSetType("");
-	if ((MouseX >= 1375) && (MouseX <= 1575) && (MouseY >= 710) && (MouseY <= 765) && (Player.CanInteract() && !DialogFocusItem.Property.Type)) InventoryItemMouthCupholderGagSetType("Cup");
-
-}
  */
-// Sets the gag type (small, cleave, otm, otn)
-function InventoryItemMouthCupholderGagSetType(NewType) {
-	var C = (Player.FocusGroup != null) ? Player : CurrentCharacter;
-	if (CurrentScreen == "ChatRoom") {
-		DialogFocusItem = InventoryGet(C, C.FocusGroup.Name);
-		InventoryItemMouthCupholderGagLoad();
-	}
-	
-	DialogFocusItem.Property.Type = NewType != "Tip" ? NewType : "";
-
-	// Refreshes the character and chatroom
-	CharacterRefresh(C);
-	var msg = "CupholderGagSet" + ((NewType) ? NewType : "NoCup");
-	var Dictionary = [];
-	Dictionary.push({ Tag: "SourceCharacter", Text: Player.Name, MemberNumber: Player.MemberNumber });
-	Dictionary.push({ Tag: "DestinationCharacter", Text: C.Name, MemberNumber: C.MemberNumber });
-	ChatRoomPublishCustomAction(msg, true, Dictionary);
-	if (DialogInventory != null) {
-		DialogFocusItem = null;
-		DialogMenuButtonBuild(C);
-	}
-}
